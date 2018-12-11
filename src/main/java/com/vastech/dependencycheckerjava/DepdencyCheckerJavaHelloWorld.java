2bb83a84366ee23c2a45a9942be3e70a1c004d13
Vijay Bheemineni <bheemineni.vijay@gmail.com>
2018-12-10 21:51:40 -0600

    Initial Commit

---
+++ src/main/java/com/vastech/dependencycheckerjava/DepdencyCheckerJavaHelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vastech.dependencycheckerjava;
+
+/**
+ *
+ * @author Vijay Bheemineni
+ */
+public class DepdencyCheckerJavaHelloWorld {
+    
+    public static void main(String args[]) {
+        System.out.println("Dependency Checker Java Hello World");
+    }
+    
+}
